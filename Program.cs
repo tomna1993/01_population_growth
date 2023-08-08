843d805712fa2aee814313e3198ae41cacfba5f5
TomasNagy <tomi.nagy@outlook.com>
2023-08-08 14:41:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PopulationGrowth
+{
+    static class Constant
+    {
+        public const int minPopulation = 9;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int startSize = 0;
+
+            do
+            {
+                Console.Write("Start size: ");
+
+                try
+                {
+                    startSize = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+
+            } while (startSize < Constant.minPopulation);
+
+
+            int endSize = 0;
+
+            do
+            {
+                Console.Write("End size: ");
+
+                try
+                {
+                    endSize = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+
+            } while (endSize >= startSize);
+        }
+    }
+}
+
